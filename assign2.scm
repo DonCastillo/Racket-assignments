aa2c79a4d4c67c476ee2237da52428e28a9a4a51
DonCastillo <don.qcastillo@yahoo.com>
2021-04-15 15:15:25 -0600

    modified file

--- assign2.scm
+++ assign2.scm
@@ -1,6 +1,10 @@
 #lang racket
 
 ; Answer to (1)
+; You are given a list whose elements are integer numbers. One example is (0
+; 0 1 3 5). Write a function, called trimming, to delete all the leading zeros. For the
+; example, the result is (1 3 5).
+
 (define (trimming n)
   (if (zero? (car n))
       (trimming (cdr n))
@@ -14,7 +18,12 @@
 (trimming '(0 0 0 1 3 5)) ; '(1 3 5)
 
 
+
 ; Answer to (2)
+; You are given a list whose elements are integer numbers. One example is (0
+; 0 1 3 0 5 0). Write a function, called trimming2, to delete all the zeros in the list. For the
+; example, the result is (1 3 5).
+
 (define (trimming2 n)
   (filter
    (lambda (x) (not (zero? x)))
@@ -27,6 +36,11 @@
 
 
 ; Answer to (3)
+; You are given a list of elements, each of which could be a list. One example
+; is ((a b) e (c d)). Create a function, called myreverse, to reverse the elements recursively.
+; For the example, the result is ((d c) e (b a)). You are not allowed to use the built-in
+; function reverse in Scheme.
+
 (define (myreverse n)
   (if (null? n)
       '()
@@ -46,6 +60,11 @@
 
 
 ; Answer to (4)
+; You are given a list of elements, each of which could be a list. One example
+; is ((a b) e (c d)). Create a function, called recslen, to count the elements recursively and
+; report the total number of the elements in the list. For the example, the result is 5. You
+; are not allowed to use the built-in function length.
+
 (define (recslen n)
   (if (null? n)
       0
